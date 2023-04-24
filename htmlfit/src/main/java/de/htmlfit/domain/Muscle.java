debb8bd50d81765d4e3ce0516422caca3471a61d
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-04-24 20:01:26 +0200

    Add files via upload

--- htmlfit/src/main/java/de/htmlfit/domain/Muscle.java
+++ htmlfit/src/main/java/de/htmlfit/domain/Muscle.java
@@ -3,10 +3,12 @@ package de.htmlfit.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import de.picsheep.domain.Album;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -20,4 +22,7 @@ public class Muscle {
 	Long id;
 	String name;
 		
+	@JsonProperty("exercise")
+	@ManyToOne()
+	private Exercise exercise;
 }
