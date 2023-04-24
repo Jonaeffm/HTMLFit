37565eca73b3b6e62d8b0543eaf53ce8e3f2b6af
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-04-24 18:40:17 +0200

    Add files via upload

---
+++ htmlfit/src/main/java/de/htmlfit/domain/Muscle.java
@@ -0,0 +1,15 @@
+package de.htmlfit.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Muscle {
+
+}
