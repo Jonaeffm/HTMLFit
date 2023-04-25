e766b0130734166c0dbd72881f5975184d8f94e8
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-04-25 19:06:50 +0200

    Add files via upload

--- htmlfit/src/main/java/de/htmlfit/htmlfit/HtmlfitApplication.java
+++ htmlfit/src/main/java/de/htmlfit/htmlfit/HtmlfitApplication.java
@@ -2,9 +2,13 @@ package de.htmlfit.htmlfit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan({"de.htmlfit.run"})
+@EnableJpaRepositories("de.htmlfit.repositories") 
+@EntityScan("de.htmlfit.domain")
 @SpringBootApplication
 public class HtmlfitApplication {
 
