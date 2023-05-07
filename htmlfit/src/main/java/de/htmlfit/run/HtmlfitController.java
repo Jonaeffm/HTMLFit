534f32ee812916b948172a8b64d0baec9ea47747
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-05-07 18:48:02 +0200

    Add files via upload
    select muscles html file

--- htmlfit/src/main/java/de/htmlfit/run/HtmlfitController.java
+++ htmlfit/src/main/java/de/htmlfit/run/HtmlfitController.java
@@ -3,36 +3,35 @@ package de.htmlfit.run;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import de.picsheep.domain.Album;
-import de.picsheep.domain.Image;
-import de.picsheep.domain.ImagePath;
+import de.htmlfit.domain.Muscle;
+import de.htmlfit.services.MuscleService;
+
 
 @Controller
 public class HtmlfitController {
-	@RequestMapping(value = "/addImage2", method = RequestMethod.GET)
+	
+	@Autowired
+	private MuscleService muscleService;
+	
+	@RequestMapping(value = "/select", method = RequestMethod.GET)
 	public String addImage2Get(Model model) {
 		
-		MuscleList = albumService.findAll();
-		model.addAttribute("albums", AlbumList);
-		return "insert2";
+		List<Muscle> muscleList;
+		muscleList = muscleService.findAll();
+		model.addAttribute("muscles", muscleList);
+		return "selectMuscle";
 	}
 
-	@RequestMapping(value = "/addImage2", method = RequestMethod.POST)
-	public String addImage2Post(@ModelAttribute("images") Image imageToAdd,
-			@ModelAttribute("imagePath") ImagePath path) {
-		Album a = albumService.findById(path.getAlbumId()).get();
-		cImage.setAlbum(a);
-		cImage.setName(imageToAdd.getName());
-		imageService.save(cImage);
-		System.out.print("test " + path.getPath());
-		System.out.println("Name: " + imageToAdd.getName());
-		System.out.println("Album: " + path.getAlbumId());
+	@RequestMapping(value = "/select", method = RequestMethod.POST)
+	public String addImage2Post(@ModelAttribute("musclesList") List<Muscle> muscles) {
+		
 		String returnStr = "redirect:/";
 		return returnStr;
 	}
