f230c8f680bf6797be3066b71be188e943579fad
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-05-07 20:30:08 +0200

    Add files via upload

--- htmlfit/src/main/java/de/htmlfit/run/HtmlfitController.java
+++ htmlfit/src/main/java/de/htmlfit/run/HtmlfitController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,5 +35,12 @@ public class HtmlfitController {
 		
 		String returnStr = "Success";
 		return returnStr;
+	}
+	
+	@GetMapping(value = "/")
+	public String test(Model model) {
+		
+		
+		return "Success";
 	}
 }
