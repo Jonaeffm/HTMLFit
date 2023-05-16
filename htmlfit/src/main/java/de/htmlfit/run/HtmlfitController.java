a94bedf2d1c9015b853f337ffbae7a1af0a9a888
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-05-16 18:24:25 +0200

    Add files via upload

--- htmlfit/src/main/java/de/htmlfit/run/HtmlfitController.java
+++ htmlfit/src/main/java/de/htmlfit/run/HtmlfitController.java
@@ -46,9 +46,9 @@ public class HtmlfitController {
 	}
 
 	@RequestMapping(value = "/select/", method = RequestMethod.POST)
-	public String addImage2Post(@ModelAttribute("muscleIds")  ArrayList<Long> muscleIds) {
+	public String addImage2Post(@ModelAttribute("muscleIds")  ArrayList<Long> muscleIdList) {
 		
-		System.out.println(muscleIds.size()+" muscles selected");
+		System.out.println(muscleIdList.size()+" muscles selected");
 		/*for(int i=0;i<muscles.size();i++) {
 			Optional<Muscle> m = muscleService.findById((long)( muscles.get(i)));
 			musclesAsObject.add(m.get());
