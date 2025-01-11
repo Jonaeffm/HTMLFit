86bed018fb55b2221cf3e458cbb38e012d602816
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2025-01-11 15:26:04 +0100

    clean
    clean init()

--- htmlfit/src/main/java/de/htmlfit/run/BootStrapData.java
+++ htmlfit/src/main/java/de/htmlfit/run/BootStrapData.java
@@ -23,8 +23,8 @@ public class BootStrapData implements CommandLineRunner{
 	ExerciseRepository er;
 	
 	public void init() {
-		Exercise burpees = new Exercise();
-		burpees.setName("Burpees");
+		
+		//-------------------muscles--------------------------
 		
 		Muscle legs = new Muscle();
 		legs.setName("legs");
@@ -54,38 +54,9 @@ public class BootStrapData implements CommandLineRunner{
 		buttocks.setName("buttocks");
 		mr.save(buttocks);
 		
-		Collection<Muscle> burpeesMuscles = new ArrayList<Muscle>();
-		burpeesMuscles.add(buttocks);
-		burpeesMuscles.add(abs);
-		burpeesMuscles.add(back);
-		burpeesMuscles.add(breast);
-		burpeesMuscles.add(triceps);
-		burpeesMuscles.add(shoulders);
-		burpeesMuscles.add(legs);
-		
-		burpees.setMuscles(burpeesMuscles);
-		er.save(burpees);
-		
-		Exercise mountainClimbers = new Exercise();
-		mountainClimbers.setName("Mountain Climbers");
-		
 		Muscle hamstrings = new Muscle();
 		hamstrings.setName("hamstrings");
 		mr.save(hamstrings);
-		
-		Collection<Muscle> mountainClimbersMuscles = new ArrayList<Muscle>();
-		mountainClimbersMuscles.add(shoulders);
-		mountainClimbersMuscles.add(triceps);
-		mountainClimbersMuscles.add(abs);
-		mountainClimbersMuscles.add(legs);
-		mountainClimbersMuscles.add(shoulders);
-		mountainClimbersMuscles.add(hamstrings);
-		
-		mountainClimbers.setMuscles(mountainClimbersMuscles);
-		er.save(mountainClimbers);
-		
-		Exercise jumpingJacks = new Exercise();
-		jumpingJacks.setName("Jumping Jacks");
 		
 		Muscle glutes = new Muscle();
 		glutes.setName("Glutes");
@@ -95,34 +66,9 @@ public class BootStrapData implements CommandLineRunner{
 		hipFlexors.setName("Hip Flexors");
 		mr.save(hipFlexors);
 		
-		Collection<Muscle> jumpingJackMuscles = new ArrayList<Muscle>();
-		jumpingJackMuscles.add(hipFlexors);
-		jumpingJackMuscles.add(glutes);
-		jumpingJackMuscles.add(legs);
-		jumpingJackMuscles.add(abs);
-		jumpingJackMuscles.add(hamstrings);
-		jumpingJackMuscles.add(shoulders);
-		jumpingJacks.setMuscles(jumpingJackMuscles);
-		er.save(jumpingJacks);
-		
-		Exercise highKnees = new Exercise();
-		highKnees.setName("High Knees");
-		
 		Muscle calves = new Muscle();
 		calves.setName("Calves");
 		mr.save(calves);
-		
-		Collection<Muscle> highKneesMuscles = new ArrayList<Muscle>();
-		highKneesMuscles.add(legs);
-		highKneesMuscles.add(glutes);
-		highKneesMuscles.add(hamstrings);
-		highKneesMuscles.add(calves);
-		
-		highKnees.setMuscles(highKneesMuscles);
-		er.save(highKnees);
-		
-		Exercise squatJumps = new Exercise();
-		squatJumps.setName("Squat Jumps");
 		
 		Muscle quads = new Muscle();
 		quads.setName("Quads");
@@ -132,20 +78,9 @@ public class BootStrapData implements CommandLineRunner{
 		biceps.setName("Biceps");
 		mr.save(biceps);
 		
-		Collection<Muscle> squatJumpsMuscles = new ArrayList<Muscle>();
-		squatJumpsMuscles.add(glutes);
-		squatJumpsMuscles.add(hamstrings);
-		squatJumpsMuscles.add(calves);
-		squatJumpsMuscles.add(quads);
-		squatJumpsMuscles.add(biceps);
-		squatJumpsMuscles.add(triceps);
-		
-		squatJumps.setMuscles(squatJumpsMuscles);
-		er.save(squatJumps);
-		
-		//__________________
-		Exercise plank = new Exercise();
-		plank.setName("Plank");
+		Muscle propel = new Muscle();
+		propel.setName("Propel");
+		mr.save(propel);
 		
 		Muscle trapezius = new Muscle();
 		trapezius.setName("Trapezius");
@@ -165,6 +100,10 @@ public class BootStrapData implements CommandLineRunner{
 		minor.setName("Minor");
 		mr.save(minor);
 		
+		Muscle pectoral = new Muscle();
+		pectoral.setName("Pectoral Muscles");
+		mr.save(pectoral);
+		
 		Muscle latissimus = new Muscle();
 		latissimus.setName("Latissimus");
 		mr.save(latissimus);
@@ -181,6 +120,83 @@ public class BootStrapData implements CommandLineRunner{
 		deltoids.setName("Deltoids");
 		mr.save(deltoids);
 		
+		//---------------------------Burpees--------------------------------
+		Exercise burpees = new Exercise();
+		burpees.setName("Burpees");
+		
+		Collection<Muscle> burpeesMuscles = new ArrayList<Muscle>();
+		burpeesMuscles.add(buttocks);
+		burpeesMuscles.add(abs);
+		burpeesMuscles.add(back);
+		burpeesMuscles.add(breast);
+		burpeesMuscles.add(triceps);
+		burpeesMuscles.add(shoulders);
+		burpeesMuscles.add(legs);
+		
+		burpees.setMuscles(burpeesMuscles);
+		er.save(burpees);
+		
+		//-----------------------Mountain Climbers--------------------------------
+		Exercise mountainClimbers = new Exercise();
+		mountainClimbers.setName("Mountain Climbers");
+		
+		Collection<Muscle> mountainClimbersMuscles = new ArrayList<Muscle>();
+		mountainClimbersMuscles.add(shoulders);
+		mountainClimbersMuscles.add(triceps);
+		mountainClimbersMuscles.add(abs);
+		mountainClimbersMuscles.add(legs);
+		mountainClimbersMuscles.add(shoulders);
+		mountainClimbersMuscles.add(hamstrings);
+		
+		mountainClimbers.setMuscles(mountainClimbersMuscles);
+		er.save(mountainClimbers);
+		
+		//-----------------------Jumping Jack--------------------------------
+		Exercise jumpingJacks = new Exercise();
+		jumpingJacks.setName("Jumping Jacks");
+		
+		Collection<Muscle> jumpingJackMuscles = new ArrayList<Muscle>();
+		jumpingJackMuscles.add(hipFlexors);
+		jumpingJackMuscles.add(glutes);
+		jumpingJackMuscles.add(legs);
+		jumpingJackMuscles.add(abs);
+		jumpingJackMuscles.add(hamstrings);
+		jumpingJackMuscles.add(shoulders);
+		jumpingJacks.setMuscles(jumpingJackMuscles);
+		er.save(jumpingJacks);
+		
+		//-----------------------High Knees--------------------------------
+		Exercise highKnees = new Exercise();
+		highKnees.setName("High Knees");
+		
+		Collection<Muscle> highKneesMuscles = new ArrayList<Muscle>();
+		highKneesMuscles.add(legs);
+		highKneesMuscles.add(glutes);
+		highKneesMuscles.add(hamstrings);
+		highKneesMuscles.add(calves);
+		
+		highKnees.setMuscles(highKneesMuscles);
+		er.save(highKnees);
+		
+		//-----------------------Squat Jumps--------------------------------
+		Exercise squatJumps = new Exercise();
+		squatJumps.setName("Squat Jumps");
+		
+		Collection<Muscle> squatJumpsMuscles = new ArrayList<Muscle>();
+		squatJumpsMuscles.add(glutes);
+		squatJumpsMuscles.add(hamstrings);
+		squatJumpsMuscles.add(calves);
+		squatJumpsMuscles.add(quads);
+		squatJumpsMuscles.add(biceps);
+		squatJumpsMuscles.add(triceps);
+		
+		squatJumps.setMuscles(squatJumpsMuscles);
+		er.save(squatJumps);
+		
+		//-----------------------Plank--------------------------------
+		Exercise plank = new Exercise();
+		plank.setName("Plank");
+		
 		Collection<Muscle> plankMuscles = new ArrayList<Muscle>();
 		plankMuscles.add(trapezius);
 		plankMuscles.add(rhomboid);
@@ -196,12 +212,9 @@ public class BootStrapData implements CommandLineRunner{
 		plank.setMuscles(plankMuscles);
 		er.save(plank);
 		
+		//-----------------------Jumps--------------------------------
 		Exercise jumps = new Exercise();
 		jumps.setName("High Jumps");
-		
-		Muscle propel = new Muscle();
-		propel.setName("Propel");
-		mr.save(propel);
 		
 		Collection<Muscle> jumpsMuscles = new ArrayList<Muscle>();
 		jumpsMuscles.add(calves);
@@ -212,6 +225,7 @@ public class BootStrapData implements CommandLineRunner{
 		jumps.setMuscles(jumpsMuscles);
 		er.save(jumps);
 		
+		//-----------------------Burpees Without Jumps--------------------------------
 		Exercise burpeesWJ = new Exercise();
 		burpeesWJ.setName("Burpees Without Jumps");
 		
@@ -222,6 +236,7 @@ public class BootStrapData implements CommandLineRunner{
 		burpeesWJ.setMuscles(bwjMuscles);
 		er.save(burpeesWJ);
 	
+		//-----------------------Split Jumps--------------------------------
 		Exercise splitJumps = new Exercise();
 		splitJumps.setName("Split Jumps");
 		Collection<Muscle> sjMuscles = new ArrayList<Muscle>();
@@ -232,12 +247,9 @@ public class BootStrapData implements CommandLineRunner{
 		splitJumps.setMuscles(sjMuscles);
 		er.save(splitJumps);
 		
+		//-----------------------Pushup--------------------------------
 		Exercise pushup = new Exercise();
 		pushup.setName("Pushup");
-		
-		Muscle pectoral = new Muscle();
-		pectoral.setName("Pectoral Muscles");
-		mr.save(pectoral);
 		
 		Collection<Muscle> pMuscles = new ArrayList<Muscle>();
 		pMuscles.add(triceps);
