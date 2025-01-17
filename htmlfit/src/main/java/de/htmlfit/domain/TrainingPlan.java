9de7ab7e441d3c2a9c236228e6a1c82247ede1ee
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2025-01-17 18:09:15 +0100

    Entity TrainingPlan added
    Training plan

--- htmlfit/src/main/java/de/htmlfit/domain/TrainingPlan.java
+++ htmlfit/src/main/java/de/htmlfit/domain/TrainingPlan.java
@@ -8,8 +8,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,21 +15,12 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data
 @NoArgsConstructor
-public class TrainingDay {
+public class TrainingPlan {
 	@Id
-	@Column(name = "TRAININGDAY_ID")
+	@Column(name = "TRAININGPLAN_ID")
 	@GeneratedValue
 	private Long id;
-	
-	@ManyToOne()
-	private TrainingPlan trainingPlan;
-
 	@JsonIgnore
-	@ManyToMany(mappedBy = "TrainingDays")
-	private Collection<Exercise> exercise;
-	
-	@JsonIgnore
-	@OneToMany(mappedBy="TrainingDay")
-	private Collection<ExerciseBuild> exerciseBuild;
-	
+	@OneToMany(mappedBy="trainingPlan")
+	private Collection<TrainingDay> trainingDays;
 }
