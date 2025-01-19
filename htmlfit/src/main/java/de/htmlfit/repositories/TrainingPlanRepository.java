d55db21a003c6ccbdbc18d6909bb79f9bb14e7c6
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2025-01-19 12:39:04 +0100

    TrainingPlanService added
    TrainingPlanService

--- htmlfit/src/main/java/de/htmlfit/repositories/TrainingPlanRepository.java
+++ htmlfit/src/main/java/de/htmlfit/repositories/TrainingPlanRepository.java
@@ -3,7 +3,8 @@ package de.htmlfit.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import de.htmlfit.domain.TrainingEquipment;
+import de.htmlfit.domain.TrainingPlan;
 
-public interface TrainingPlanRepository extends JpaRepository<TrainingEquipment, Long>{
+public interface TrainingPlanRepository extends JpaRepository<TrainingPlan, Long>{
 
 }
