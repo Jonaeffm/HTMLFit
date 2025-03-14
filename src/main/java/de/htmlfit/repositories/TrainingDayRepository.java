94ec0c73a075a8a823ce0b48abddd2c7c5d39a18
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2025-03-14 17:35:33 +0100

    Delete src directory

--- src/main/java/de/htmlfit/repositories/TrainingDayRepository.java
+++
@@ -1,9 +0,0 @@
-package de.htmlfit.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import de.htmlfit.domain.TrainingDay;
-
-public interface TrainingDayRepository extends JpaRepository<TrainingDay, Long>{
-
-}
