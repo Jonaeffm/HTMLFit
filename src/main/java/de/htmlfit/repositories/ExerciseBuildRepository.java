c535a923e5f6c61d7d952ca11af06a0e13d309ae
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2025-05-25 13:56:51 +0200

    Delete src directory

--- src/main/java/de/htmlfit/repositories/ExerciseBuildRepository.java
+++
@@ -1,9 +0,0 @@
-package de.htmlfit.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import de.htmlfit.domain.ExerciseBuild;
-
-public interface  ExerciseBuildRepository  extends JpaRepository<ExerciseBuild, Long> {
-
-}
