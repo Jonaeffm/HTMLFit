c6315d8b1752d9fc35d4e0119aee869d2c59c2ce
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2026-03-19 19:59:09 +0100

    new methods

--- htmlfit/src/main/java/de/htmlfit/services/MethodService.java
+++ htmlfit/src/main/java/de/htmlfit/services/MethodService.java
@@ -3,6 +3,8 @@ package de.htmlfit.services;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import de.htmlfit.domain.Exercise;
 import de.htmlfit.domain.Muscle;
 import de.htmlfit.domain.TrainingDay;
@@ -10,10 +12,27 @@ import de.htmlfit.domain.TrainingEquipment;
 
 public class MethodService{
 	
+	@Autowired
+	private ExerciseService exerciseService;
+	
 	public ArrayList<Exercise> hiitExercises(TrainingDay td) {
 		
 		Collection<Muscle> selectedMuscles = td.getMuscles(); 
 		Collection<TrainingEquipment> selectedEquipment = td.getEquip();
+	
+		Collection<Exercise> allExercises = exerciseService.findAll();
+		
+		for(int i=0;i<4;i++) {
+			int j=0;
+			for(Exercise e : allExercises) {
+				for (Muscle m: selectedMuscles) {
+					if (e.getMuscles().contains(m)) {
+						j++;
+					}
+				}
+			}
+			
+		}
 		
 		return null;
 		
