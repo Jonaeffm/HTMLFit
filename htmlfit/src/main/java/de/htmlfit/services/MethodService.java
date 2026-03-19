3968ebdd3134b48aec0e02a6c1256d34bdb6c857
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2026-03-19 19:44:04 +0100

    new method
    method which selects the hiit exercises (not done complete)

--- htmlfit/src/main/java/de/htmlfit/services/MethodService.java
+++ htmlfit/src/main/java/de/htmlfit/services/MethodService.java
@@ -1,5 +1,22 @@
 package de.htmlfit.services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import de.htmlfit.domain.Exercise;
+import de.htmlfit.domain.Muscle;
+import de.htmlfit.domain.TrainingDay;
+import de.htmlfit.domain.TrainingEquipment;
+
 public class MethodService{
+	
+	public ArrayList<Exercise> hiitExercises(TrainingDay td) {
+		
+		Collection<Muscle> selectedMuscles = td.getMuscles(); 
+		Collection<TrainingEquipment> selectedEquipment = td.getEquip();
+		
+		return null;
+		
+	}
 	
 }
